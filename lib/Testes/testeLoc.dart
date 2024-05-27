31d461cad89330bf7af606e1dd859bdbaa37c049
Matheus Kansbock <matheus.kansbock@gmail.com>
2024-05-27 08:18:56 -0300

    sprint2

--- lib/Testes/testeLoc.dart
+++ lib/Testes/testeLoc.dart
@@ -5,12 +5,12 @@ import 'package:test/test.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  test("", () {
+  test("Testar a determinação da localização.", () {
     Coordenadas loc = Coordenadas();
     var localizacao = loc.converterLoc(-23.6468175, -46.5730710);
     expect(localizacao.x < 30 && localizacao.x >= 0 && localizacao.y < 60 && localizacao.y >= 0, true );
   });
-  test("", () async {
+  test("Testar para ver se ao obter coordenadas fora do ginásio ele retorna as coordenadas da entrada.", () async {
     Coordenadas loc = Coordenadas();
     await loc.getPosicao();
     var localizacao = loc.converterLoc(loc.lat, loc.long);
